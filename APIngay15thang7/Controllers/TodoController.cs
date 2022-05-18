ad028601041527679415f471444539825ae4d1e0
honqphonq <lehongphong1103@gmail.com>
2022-05-18 09:52:15 +0700

    Add method Api

---
+++ APIngay15thang7/Controllers/TodoController.cs
@@ -0,0 +1,78 @@
+﻿using APIngay15thang7.Data;
+using APIngay15thang7.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIngay15thang7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+        }
+        private static List<TodoList> todos = new List<TodoList> { 
+            new TodoList{Id = 1, Content = "dingu"}, 
+            new TodoList{Id = 2, Content = "dingu2"}, 
+            new TodoList{Id = 4, Content = "dingu3"}, 
+            };
+        [HttpGet]
+        public async Task<ActionResult<List<TodoList>>> GetAll()
+        {
+           return Ok(await _context.TodoLists.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var todo = await _context.TodoLists.Where(x => x.Id == id).FirstAsync();
+            if(todo == null)
+                return NotFound();
+            return Ok(todo);
+        }
+        [HttpPost]
+        public async Task<ActionResult<List<TodoList>>> Add([FromForm]TodoList item)
+        {
+            _context.TodoLists.Add(item);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.TodoLists.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Update(TodoList request)
+        {
+            var todo = await _context.TodoLists.FindAsync(request.Id);
+            if (todo == null)
+            {
+                return BadRequest();
+            }
+            todo.Content = request.Content;
+            todo.Id = request.Id;
+            _context.TodoLists.Update(todo);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.TodoLists.ToListAsync());
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var todo = await _context.TodoLists.Where(p => p.Id == id).FirstOrDefaultAsync();
+            if(todo == null)
+            {
+                return BadRequest();
+            }
+            _context.TodoLists.Remove(todo);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.TodoLists.ToListAsync());
+            
+        }
+
+
+
+    }
+}
